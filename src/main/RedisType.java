8c3fa7c03386b537f622ae2ee4d648e3f1731791
necmettin <necmettinuzun5@gmail.com>
2020-06-25 23:19:45 +0300

    RedisType added

---
+++ src/main/RedisType.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.Optional;
+
+public enum RedisType {
+
+
+	IMSI_IMEI(0,"IMSI_IMEI"),
+	IMSI_MSISDN(1,"IMSI_MSISDN"),
+	MSISDN_IMSI(2,"MSISDN_IMSI");
+	
+	private int id;
+	private String name;
+
+	 RedisType(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	 
+	 public int getId() {
+		 return this.id;
+	 }
+	 
+	 public String getName() {
+		 return this.name;
+	 }
+	 
+	 public static Optional<RedisType> getRedisType(int id) {
+		 for (RedisType redisType : RedisType.values()) {
+			if(id == redisType.getId()) {
+				return Optional.of(redisType);
+			}
+		}
+		 return Optional.empty();
+	 }
+}
--- src/main/Starter.java
+++ src/main/Starter.java
@@ -1,9 +1,44 @@
 package main;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Starter {
 
 	public static void main(String[] args) {
-		System.out.println("TEST");
+
+		System.out.println("Redis Type:");
+		printRedisTypeInfo();
+
+		try (Scanner in = new Scanner(System.in);) {
+
+			Optional<RedisType> redisType = RedisType.getRedisType(in.nextInt());
+
+			if (redisType.isEmpty()) {
+				System.out.println("Redis Type not found!!");
+				return;
+			}
+
+			switch (redisType.get()) {
+			case IMSI_IMEI:
+				break;
+			case IMSI_MSISDN:
+				break;
+			case MSISDN_IMSI:
+				break;
+			default:
+				System.out.println("Redis type not found!!" + redisType.get().getName());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void printRedisTypeInfo() {
+
+		for (RedisType redisType : RedisType.values()) {
+			System.out.println(redisType.getName() + " : " + redisType.getId());
+		}
 
 	}
 
